5fd1f6503c959cc60f7acc3316709493a5d383e9
@Avanish_V <121923886+Avanish-V@users.noreply.github.com>
2024-11-08 10:03:40 +0530

    29/10/2024

--- src/main/kotlin/example/com/Cloudinary.kt
+++ src/main/kotlin/example/com/Cloudinary.kt
@@ -3,4 +3,8 @@ package com.example
 import com.cloudinary.Cloudinary
 import com.cloudinary.utils.ObjectUtils
 
-
+val cloudinary = Cloudinary(mapOf(
+    "cloud_name" to "dni4h8jjy",
+    "api_key" to "188248912913531",
+    "api_secret" to "33aVFphaBuJoU7CIz4MGcbD2MRc"
+))
